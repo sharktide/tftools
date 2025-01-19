6bc21fccd59196bd765fb3afc4c7d85f6ccb05cd
sharktide <meherrihaan@gmail.com>
2025-01-18 21:57:26 -0500

    0.0.2

--- src/tensorflowtools/__init__.py
+++ src/tensorflowtools/__init__.py
@@ -1,10 +1,11 @@
 # Expose key functions for easy access
-from .hftools import download_model_from_huggingface
-from .kerastools import load_model_from_cache
+from .hftools import download_model_from_huggingface, clear_model_cache
+from .kerastools import load_from_hf_cache
 
 __all__ = [
     "download_model_from_huggingface",  # Expose this function when importing tftools
-    "load_model_from_cache",            # Expose this function as well
+    "clear_model_cache",
+    "load_from_hf_cache",            # Expose this function as well
 ]
 
 
